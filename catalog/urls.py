a6e24a6850074b592244e9e1bbcb6ea83f3d21ed
margot07008 <9311margarita@gmail.com>
2020-03-08 01:31:49 +0300

    add settings

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('ask/', views.ask_question, name='ask'),
     path('views.py', views.add_new_question, name='add_new_question'),
     path('question/', views.question_page, name='question'),
+    path('settings/', views.profile_settings, name='settings'),
 ]
