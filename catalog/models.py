8bba072decd8d979dde6dd8da0c69c01c97721f0
margot07008 <9311margarita@gmail.com>
2020-04-05 20:37:34 +0300

    krasota

--- catalog/models.py
+++ catalog/models.py
@@ -1,8 +1,6 @@
-# from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.db.models import Sum
 from django.utils import timezone
 
 from core.models import User
@@ -31,21 +29,10 @@ class Tag(models.Model):
 
 
 class LikeDislikeManager(models.Manager):
-    use_for_related_fields = True
-
-    def likes(self):
-        # Забираем queryset с записями больше 0
-        return self.get_queryset().filter(vote__gt=0)
-
-    def dislikes(self):
-        # Забираем queryset с записями меньше 0
-        return self.get_queryset().filter(vote__lt=0)
-
-    def sum_rating(self):
-        # Забираем суммарный рейтинг
-        return self.get_queryset().aggregate(Sum('vote')).get('vote__sum') or 0
+    pass
 
 class LikeDislike(models.Model):
+    objects = LikeDislikeManager()
     LIKE = 1
     DISLIKE = -1
 
@@ -53,21 +40,12 @@ class LikeDislike(models.Model):
         (DISLIKE, 'Не нравится'),
         (LIKE, 'Нравится')
     )
-
-    vote = models.SmallIntegerField(verbose_name=("Голос"), choices=VOTES)
-    user = models.ForeignKey(User, verbose_name=("Пользователь"), on_delete=models.CASCADE)
-
+    vote = models.SmallIntegerField(choices=VOTES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #ContentType фреймворк
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
-
-    objects = LikeDislikeManager()
-
-    def articles(self):
-        return self.get_queryset().filter(content_type__model='question').order_by('-create_data')
-
-    def comments(self):
-        return self.get_queryset().filter(content_type__model='answer').order_by('-create_data')
+    obj = GenericForeignKey('content_type', 'object_id')
 
 
 class QuestionManager(models.Manager):
